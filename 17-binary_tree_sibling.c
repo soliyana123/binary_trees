fcc83d876e08769bf2ecb4bf4f0ebd42d88627af
soliye <soliy@gmail.com>
2022-09-20 22:46:50 -0700

    siblings

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,21 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: Binary tree
- * Description: checks if a binary tree is perfect
- * Return: 1 or 0
+ * binary_tree_sibling -  finds the sibling of a node
+ * @node: the node
+ * Description:  finds the sibling of a node
+ * Return: sibling node or null
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *sibling = NULL;
-	binary_tree_t *parent = NULL;
-
-	if (node == NULL || node->parent == NULL)
+	if (!node)
 		return (NULL);
-	parent = node->parent;
-	if ((node == parent->left) && (parent->right != NULL))
-		sibling = parent->right;
-	else if ((node == parent->right) && (parent->left != NULL))
-		sibling = parent->left;
-	return (sibling);
+	if (node->parent && node->parent->left != node)
+		return (node->parent->left);
+	else if (node->parent && node->parent->right != node)
+		return (node->parent->right);
+	return (NULL);
 }
